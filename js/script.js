c2e26a4a461c866411acc457e034f5e3933e64e1
Immolazione <federico.deered@gmail.com>
2022-02-04 14:18:32 +0100

    add base structure

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log('VUE OK', Vue);
+console.log('AXIOS OK', axios);
+
+Vue.config.devtools = true;
+
+/*
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+Usando Vue.js, generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+Evitare i doppioni nella lista delle mail
+*/
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        emails: [],
+    },
+    
+});
